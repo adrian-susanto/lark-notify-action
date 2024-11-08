6fd4860e961208f07d8758fa909d833b4274a784
root <root@DESKTOP-378PAQS>
2024-11-08 14:59:43 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core')
-const { wait } = require('./wait')
+const github = require('@actions/github')
+const axios = require('axios')
 
 /**
  * The main function for the action.
@@ -7,22 +8,140 @@ const { wait } = require('./wait')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    const hookToken = core.getInput('hookToken', { required: true })
+    const secret = core.getInput('secret')
+    const platform = core.getInput('platform') || 'lark'
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`hookToken: ${hookToken}`)
+    core.debug(`secret: ${secret}`)
+    core.debug(`platform: ${platform}`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    let url = ''
+    if (platform === 'feishu') {
+      url = 'https://open.feishu.cn/open-apis/bot/v2/hook/'
+    } else {
+      url = 'https://open.larksuite.com/open-apis/bot/v2/hook/'
+    }
+    url = `${url}${hookToken}`
+    core.debug(`url: ${url}`)
+    const timestamp = Date.now()
+    let sign = '' // 初始化 sign 变量
+    // 签名
+    if (secret.length > 0) {
+      sign = genSign(timestamp, secret)
+      core.debug(`sign ${sign}`)
+    }
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const bodyString = {
+      timestamp,
+      sign,
+      msg_type: 'interactive',
+      card: {
+        config: {
+          wide_screen_mode: true
+        },
+        header: {
+          template: core.getInput('title_color'),
+          title: {
+            tag: 'plain_text',
+            content: core.getInput('title')
+          }
+        },
+        elements: [
+          {
+            tag: 'markdown',
+            content: `${core.getInput('message') || ''} <at id=all></at>`
+          },
+          {
+            tag: 'div',
+            fields: [
+              {
+                is_short: true,
+                text: {
+                  tag: 'lark_md',
+                  content: '**Ref：**\n报事报修'
+                }
+              },
+              {
+                is_short: true,
+                text: {
+                  tag: 'lark_md',
+                  content: '**Event：**\n客服工单'
+                }
+              }
+            ]
+          },
+          {
+            tag: 'div',
+            fields: [
+              {
+                is_short: true,
+                text: {
+                  tag: 'lark_md',
+                  content: '**Author:**\naaaa'
+                }
+              },
+              {
+                is_short: true,
+                text: {
+                  tag: 'lark_md',
+                  content: '**Time：**\n2024/1/16'
+                }
+              }
+            ]
+          },
+          {
+            tag: 'div',
+            text: {
+              content: '这是一段普通文本😄',
+              tag: 'lark_md'
+            }
+          },
+          {
+            tag: 'action',
+            actions: [
+              {
+                tag: 'button',
+                text: {
+                  tag: 'plain_text',
+                  content: '查看详情'
+                },
+                type: 'primary',
+                multi_url: {
+                  url: 'https://open.larksuite.com/document',
+                  android_url: '',
+                  ios_url: '',
+                  pc_url: ''
+                }
+              }
+            ]
+          }
+        ]
+      }
+    }
+
+    const resp = await axios.post(url, bodyString)
+
+    core.debug(`resp: ${JSON.stringify(resp)}`)
+
+    if (resp.errcode > 0) {
+      core.info(`✅ [DONE] ${resp.errmsg}`)
+    } else {
+      core.setFailed(`❌ [FAILED] ${resp.errmsg}`)
+    }
   } catch (error) {
+    core.notice(`error ${error.message}`)
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
   }
+}
+function genSign(timestamp, secret) {
+  const stringToSign = `${timestamp}\n${secret}`
+
+  const hmac = crypto.createHmac('sha256', secret)
+  hmac.update(stringToSign)
+
+  return hmac.digest('base64')
 }
 
 module.exports = {
